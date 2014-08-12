3d4bd4bf2ce425163d6397b0ad443f6bf29219df
Adrien Philippe <aphilippe@clever-age.com>
2014-08-12 13:57:53 +0200

    add input manager

---
+++ src/Managers/InputManager/InputManager.cpp
@@ -0,0 +1,9 @@
+//
+//  InputManager.cpp
+//  RPGEvolve
+//
+//  Created by Adrien Philippe on 12/08/14.
+//  Copyright (c) 2014 aphilippe. All rights reserved.
+//
+
+#include "InputManager.h"
---
+++ src/Managers/InputManager/InputManager.h
@@ -0,0 +1,14 @@
+//
+//  InputManager.h
+//  RPGEvolve
+//
+//  Created by Adrien Philippe on 12/08/14.
+//  Copyright (c) 2014 aphilippe. All rights reserved.
+//
+
+#ifndef __RPGEvolve__InputManager__
+#define __RPGEvolve__InputManager__
+
+#include <iostream>
+
+#endif /* defined(__RPGEvolve__InputManager__) */
